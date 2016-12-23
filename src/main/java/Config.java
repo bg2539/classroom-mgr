3c09ea9a49b7fe24e7186d412167119455fdb06b
Bo Gan <gan.bo@columbia.edu>
2016-12-22 22:42:31 -0500

    purge

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -9,29 +9,9 @@ public class Config {
 	static final String Github_Org =  /*"bg2539-github-api-test";*/ "W4118";
 	static final String Github_Org_Staging = "W4118-grade";
 	static final GitHubClient AdminClient = new GitHubClient().setOAuth2Token("cb56fb889487a7d258e203f1ea0d269fbea9aadf");
-	static final String User_DB = System.getProperty("user.home") + "/.github_classroom";
-
 	static final class UserComp implements Comparator<User> {
 		public int compare(User a, User b) {
 			return a.getLogin().compareTo(b.getLogin());
 		}
-	}
-
-	static Map<String, String> loadUsers() throws Exception {
-		Map<String, String> db = null;
-		FileInputStream fis = new FileInputStream(User_DB);
-		ObjectInputStream ois = new ObjectInputStream(fis);
-		db = (Map<String, String>) ois.readObject();
-		ois.close();
-		fis.close();
-		return db;
-	}
-
-	static void StoreUsers(Map<String, String> db) throws Exception {
-		FileOutputStream fos = new FileOutputStream(User_DB);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(db);
-		oos.close();
-		fos.close();
 	}
 }
--- src/main/java/RemoveForkRepos.java
+++ src/main/java/RemoveForkRepos.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
 import java.util.List;
 
-import org.eclipse.egit.github.core.Repository;
-
 public class RemoveForkRepos {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] arg) throws IOException {
+		Utils.repo_serv_admin.deleteRepository(Config.Github_Org_Staging, arg[0]);
 	}
 
 }
